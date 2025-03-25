5d7dd0bdd6737c082ad0bb1e1b27bbaab6530c79
BugHunter <76444458+BugHunter021@users.noreply.github.com>
2025-03-25 13:32:13 +0330

    Update linkfinder.py

--- linkfinder.py
+++ linkfinder.py
@@ -257,10 +257,10 @@ def html_save(html):
 
         print("URL to access output: file://%s" % os.path.abspath(args.output))
         file = "file:///%s" % os.path.abspath(args.output)
-        if sys.platform == 'linux' or sys.platform == 'linux2':
-            subprocess.call(["xdg-open", file])
-        else:
-            webbrowser.open(file)
+        #if sys.platform == 'linux' or sys.platform == 'linux2':
+        #    subprocess.call(["xdg-open", file])
+        #else:
+        #    webbrowser.open(file)
     except Exception as e:
         print("Output can't be saved in %s \
             due to exception: %s" % (args.output, e))
